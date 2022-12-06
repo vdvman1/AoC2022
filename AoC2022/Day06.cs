edc8e6de9c2c11a33d152ff7ed75687978062ee2
Stefan VDV <vdvman88@gmail.com>
2022-12-06 16:54:30 +1100

    Day 6

---
+++ AoC2022/Day06.cs
@@ -0,0 +1,62 @@
+﻿namespace AoC2022;
+
+public partial class Day06 : DayBase
+{
+    public override void ParseData() { }
+
+    [Benchmark]
+    public override string Solve1()
+    {
+        ReadOnlySpan<byte> chars = Contents;
+        int start = 0;
+        int i = 1;
+        do
+        {
+            for (int j = i - 1; j >= start; j--)
+            {
+                if (chars[i] == chars[j])
+                {
+                    start = j + 1;
+                    break;
+                }
+            }
+
+            if (i - start == 3)
+            {
+                return (i + 1).ToString();
+            }
+
+            ++i;
+        } while (i < chars.Length);
+
+        return "No start markers found";
+    }
+
+    [Benchmark]
+    public override string Solve2()
+    {
+        ReadOnlySpan<byte> chars = Contents;
+        int start = 0;
+        int i = 1;
+        do
+        {
+            for (int j = i - 1; j >= start; j--)
+            {
+                if (chars[i] == chars[j])
+                {
+                    start = j + 1;
+                    break;
+                }
+            }
+
+            if (i - start == 13)
+            {
+                return (i + 1).ToString();
+            }
+
+            ++i;
+        } while (i < chars.Length);
+
+        return "No start markers found";
+    }
+}
